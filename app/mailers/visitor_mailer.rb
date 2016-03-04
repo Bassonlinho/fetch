27bd20b075bdbc8c8917092ed82d3c05a6a725a7
DevKolosek <dev@kolosek.com>
2016-03-04 13:24:18 +0100

    Changed the default form address from 'fedja@kolosek.com' to 'noreply@check-kolosek.com'

--- app/mailers/visitor_mailer.rb
+++ app/mailers/visitor_mailer.rb
@@ -3,8 +3,7 @@ class VisitorMailer < ApplicationMailer
   def contact_email(email)
     @email = email
     @message = "Your task is completed"
-    mail(#from: @email,
-         from: 'fedja@kolosek.com',
+    mail(from: 'noreply@check-kolosek.com',
          to: @email,
          subject: 'Your task has been completed')
   end
