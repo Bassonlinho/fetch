5a57d879e3cabff2f725face46168da8764b5e48
FKolosek <fedja@kolosek.com>
2016-05-12 10:05:18 +0200

    More console output

--- app/services/url_checker.rb
+++ app/services/url_checker.rb
@@ -23,22 +23,32 @@ module UrlChecker
 
 	def self.find_words_in_text(words, text)
 		if words.kind_of?(Array)
+			p 'on line 26 in url_checker.rb'
 			words.each do |w| 
+				p 'on line 28 in url_checker.rb'
 				#text.match(/#{w}/) ? true : false
 				if text.downcase.include? w.downcase
+					p 'on line 31 in url_checker.rb'
 					return true
 					break
 				else
+					p 'on line 35 in url_checker.rb'
 					return false
 				end
 			end
 		elsif words.kind_of?(String)
+			p 'on line 40 in url_checker.rb'
 			#text.match(/words/) ? true : false
 			if text.include? words
+				p 'on line 43 in url_checker.rb'
 				true
 			else
+				p 'on line 46 in url_checker.rb'
 				false
 			end
+		else
+			p 'on line 50 in url_checker.rb'
+			return true
 		end
 	end	
 end
